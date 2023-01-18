5b02bd04181a229848b0bdfa5e9c986f7eed77e8
anas599 <anas599@yahoo.com>
2023-01-18 21:45:48 +0300

    Add function to add task

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,37 +1,41 @@
 import './style.css';
+import addTask from './listFunctions.js';
 
+export const listData = [
+  {
+    description: 'Wash the car',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Pay the all bills',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go to the Gym',
+    completed: true,
+    index: 3,
+  },
+];
+const containerDiv = document.createElement('div');
+containerDiv.classList.add('add-container');
+const element = document.querySelector('#toDoList');
+const addToDo = document.createElement('p');
+const addPlaceHolder = document.createElement('input');
+export const showList = document.createElement('div');
+const clearAll = document.createElement('a');
+const addButton = document.createElement('button');
+addButton.id = 'addTask1';
+addButton.innerHTML = 'Add1';
 function component() {
-  const element = document.querySelector('#toDoList');
-  const addToDo = document.createElement('p');
-  const x = document.createElement('input');
-  const addPlaceHolder = document.createElement('input');
-  const showList = document.createElement('div');
-  const clearAll = document.createElement('a');
-  addToDo.innerHTML = 'Today\'s To Do';
+  addToDo.innerHTML = "Today's To Do";
   addToDo.classList.add('todayHead');
   addPlaceHolder.classList.add('addPlaceHolder');
   addPlaceHolder.setAttribute('placeholder', 'Add to your list...');
+  addPlaceHolder.setAttribute('type', 'text');
   clearAll.innerHTML = 'Clear all completed';
   clearAll.classList = 'clearAll';
-  x.setAttribute('type', 'checkbox');
-  x.id = 'myInput';
-  const listData = [
-    {
-      description: 'Wash the car',
-      completed: true,
-      index: 2,
-    },
-    {
-      description: 'Pay the bills',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Go to the Gym',
-      completed: true,
-      index: 3,
-    },
-  ];
 
   // Lodash, now imported by this script
 
@@ -52,9 +56,16 @@ function component() {
 
   element.classList.add('toDoList');
   element.appendChild(addToDo);
-  element.appendChild(addPlaceHolder);
+  containerDiv.appendChild(addPlaceHolder);
+  containerDiv.appendChild(addButton);
+  element.appendChild(containerDiv);
   element.appendChild(showList);
   element.appendChild(clearAll);
+
+  document.querySelector('#addTask1').addEventListener('click', () => {
+    addTask(document.querySelector('.addPlaceHolder').value);
+  });
+
   return element;
 }
 
--- webpack-demo/src/listFunctions.js
+++ webpack-demo/src/listFunctions.js
@@ -1,9 +1,26 @@
-const taskList = [];
-function addTask() {
-  let count = 0;
-  const taskInput = document.getElementById('myInput');
-  const taskText = taskInput.value;
-  const task = { text: taskText, completed: false, index: count++ };
-  taskList.push(task);
-  taskInput.value = '';
+// eslint-disable-next-line import/no-cycle
+import { listData, showList } from './index.js';
+
+export default function addTask(description) {
+  const nextIndex = listData.length + 1;
+  const newTask = {
+    description,
+    completed: false,
+    index: nextIndex,
+  };
+  listData.push(newTask);
+  showList.innerHTML = listData
+    .map(
+      (toDoArr) => `
+      <li class='oneTask'>
+      <p id='indexNum'>${toDoArr.index} </p>
+      <p>${toDoArr.description} </p>
+      <label for="checkbox"> </label>
+      <input type='checkbox' id="checkbox">
+      <i class="threeDots"> .</i>
+    </li>
+    `,
+    )
+    .sort()
+    .join('');
 }
